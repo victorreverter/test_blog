235e636de6a448527c33537765973bdc4021b152
Víctor Manuel Herrera <vmherrera@vmherrera-macbook.local>
2024-01-03 10:34:20 +0100

    First Build V

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Search from '../components/Search'
+import IntroPost from '../components/IntroPost'
+import Blogs from '../components/Blogs'
+import GlobalData from '../assets/blogpost_data.json'
+
+
+function Home() {
+  const PostsData = GlobalData.blogposts;
+
+  // console.log(GlobalData.blogposts.length);
+  // console.log(PostsData);
+
+  return (
+    <div id='home_container'>
+      {/* Search */}
+      <Search />
+      {/* IntroPost */}
+      {/* <IntroPost /> */}
+      {PostsData.length>0 ? <IntroPost PostData={PostsData[0]} /> :null}
+      {/* Blogs */}
+      {/* <Blogs />   */}
+      {PostsData.length>0 ? <Blogs PostData={PostsData} /> :null}
+    </div>
+  )
+}
+
+export default Home
