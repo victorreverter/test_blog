79dd8a8cfb06006ae5341420ca26a8eb3b0530ec
Víctor Manuel Herrera <vmherrera@vmherrera-macbook.local>
2024-01-03 13:06:29 +0100

    Trying to solve loading 1.9

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/test-blog/" element={<Home/>}></Route>
-          <Route path="/test-blog/blog-detail/:id" element={<BlogDetail/>}></Route>
+          <Route path="/test_blog/" element={<Home/>}></Route>
+          <Route path="/test_blog/blog-detail/:id" element={<BlogDetail/>}></Route>
         </Routes>
 
         {/* Footer */}
